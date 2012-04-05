317f0ddb072c4d86b80fa871faa2a07d55c957fd
Carl Masak <cmasak@gmail.com>
2012-04-05 22:29:02 +0200

    [Sambal] a &text call results in a Slide with a Text

--- lib/Sambal.pm
+++ lib/Sambal.pm
@@ -1,12 +1,20 @@
 module Sambal;
 
+class Text {
+}
+
 class Slide {
+    has @.children;
+
+    method new(*@nodes) {
+        self.bless(*, :children(@nodes));
+    }
 }
 
 my @slide_queue;
 
 sub text(Cool $text) is export {
-    push @slide_queue, Slide.new;
+    push @slide_queue, Slide.new(Text.new);
 }
 
 our sub slides {
